e761680d87baf74a69baf22740783fe314b9b722
PrettyPrimate <davidbubb1@gmail.com>
2015-08-02 15:18:10 +0100

    first

---
+++ PrettyPrimate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrettyPrimate.Startup))]
+namespace PrettyPrimate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
